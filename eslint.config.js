c66db6640397ff6d512886ace44018eedbbeb357
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-10-17 03:21:25 +0200

    chore(eslint): disable react-in-jsx-scope rule for React 17+ JSX Transform

--- eslint.config.js
+++ eslint.config.js
@@ -9,4 +9,14 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Disable the rule that requires React in JSX scope
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
 ];

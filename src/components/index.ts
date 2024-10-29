86faad8e65db75c94c645d488b95b541131e926f
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-10-29 23:09:10 +0100

    chore(components): centralize component exports for cleaner imports

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import NavBar from './Navbar/Navbar';
+import ProductList from './ProductList/ProductList';
+import Product from './ProductList/Product';
+import Footer from './Footer/Footer';
+import Cart from './Cart/Cart';
+import IconButton from './IconButton/IconButton';
+import SvgIcon from './SvgIcon/SvgIcon';
+import Dashboard from './Dashboard/Dashboard';
+
+export {
+  NavBar,
+  ProductList,
+  Product,
+  Footer,
+  Cart,
+  IconButton,
+  SvgIcon,
+  Dashboard,
+};

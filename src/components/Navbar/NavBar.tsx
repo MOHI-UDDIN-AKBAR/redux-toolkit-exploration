4f645717bc6f05fc16e909c3acfa00cc7b3afb32
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-11-13 14:41:17 +0100

    feat(navbar): add simple NavBar component

---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,13 @@
+import './NavBar.css';
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav-group">
+        <li className="nav-link">Cool Products</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
